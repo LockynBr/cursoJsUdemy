0888909292f409501514d912a8ec0a6d9e345007
Lockynbr <pauloluz566@gmail.com>
2024-08-29 18:28:58 -0300

    introdução de funções

---
+++ aulas/aula31/index.js
@@ -0,0 +1,16 @@
+/* function soma(a = 1, b = 1) { //As variáveis podem ter valores defalut.
+    return a + b;
+}
+
+console.log(soma(4, 2)) */
+
+// const raiz = function (n) { //Função anônima
+//     return n ** 0.5;
+// };
+
+const raiz = n => n ** 0.5; //Arrow Function
+
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
