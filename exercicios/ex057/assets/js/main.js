a4cbc2c6e234f36656f2e47bef773aad72aac66e
Lockynbr <pauloluz566@gmail.com>
2024-08-29 18:52:39 -0300

    exercicio com NodeList

---
+++ exercicios/ex057/assets/js/main.js
@@ -0,0 +1,10 @@
+const containerParagrafos = document.querySelector('.container-paragrafos');
+const paragrafos = document.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+
+for (let p of paragrafos) {
+    p.style.backgroundColor = backgroundColorBody;
+    p.style.color = '#fff';
+}
