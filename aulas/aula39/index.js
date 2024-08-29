2313ff20ac677a3bc6dd1079cc3020e6a2745328
Lockynbr <pauloluz566@gmail.com>
2024-08-29 18:30:33 -0300

    curto-circuito (short-circuit)

---
+++ aulas/aula39/index.js
@@ -0,0 +1,29 @@
+/*
+AND - && -> false && true -> false "o valor mesmo"
+OR -|| -> true || false -> vai retornar "o valor verdadeiro"
+
+FALSY VALUE
+false
+0
+'' "" ``
+null / undefined
+NaN
+*/
+
+//AND
+/* function falaOi() {
+    return 'Oi';
+}
+
+const vaiExecutar = 'Joãozinho';
+
+console.log(vaiExecutar && falaOi()); */
+
+//OR
+//console.log(0 || false || null || "Paulo Henrique" || true);
+
+/* const corUsuario = 'Vermelho';
+const corPadrao = corUsuario || 'Preto';
+
+console.log(corPadrao);
+ */
