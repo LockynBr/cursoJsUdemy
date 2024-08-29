bc47141d00cb7076bbfdf3c688b9476d2be0690d
Lockynbr <pauloluz566@gmail.com>
2024-08-29 18:28:15 -0300

    objeto math

---
+++ aulas/aula27/index.js
@@ -0,0 +1,14 @@
+let num1 = 9.54578;
+//let num2 = Math.floor(num1); //Arredonda o Número para baixo
+//let num2 = Math.ceil(num1); //Arredonda o Número para cima
+//let num2 = Math.round(num1); //Arredonda para o mais próximo
+//console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); //Pega o maior número da sequência
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); //Pega o menor número da sequência
+//const aleatorio = Math.random(); //Gera um número aleatório entre 0 e 1
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+
+console.log(Math.pow(2, 10)) //Faz potenciação
+
+//Raiz quadrada
+let n1 = 16;
+console.log(n1 ** (1/2))
