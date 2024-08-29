ce9cc416104fd015ce08093f2f2be9485f7cbf3d
Lockynbr <pauloluz566@gmail.com>
2024-08-29 18:44:07 -0300

    forEach

---
+++ aulas/aula88/index.js
@@ -0,0 +1,14 @@
+const a1 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+a1.forEach(function(valor) {
+    console.log(valor);
+});
+console.log(total)
+
+// ForEach arrow function
+//a1.forEach(valor => console.log(valor));
+
+// Forma de usar reduce com forEach
+// let total = 0;
+// a1.forEach(valor => {
+//     total += valor;
+// });
