df40ce8d04d6b65dfa0aebdd58a1236f295f47f6
Lockynbr <pauloluz566@gmail.com>
2024-08-29 18:42:13 -0300

    review array basico

---
+++ aulas/aula81/index.js
@@ -0,0 +1,12 @@
+//const nomes = new Array('Eduardo', 'Maria', 'Joana'); //Podemos criar arrays utilizando a forma de consturores.
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+// const novo = nomes.slice(1, -2);
+// console.log(novo);
+
+//const novo = [...nomes]; //Spread Operator
+
+// const nome = 'Paulo Henrique Luz';
+// const nomes = nome.split(' ');
+const nomes = [ 'Paulo', 'Henrique', 'Luz' ];
+const nome = nomes.join(' '); //Serve para juntar coisas de um array.
+console.log(nome);
