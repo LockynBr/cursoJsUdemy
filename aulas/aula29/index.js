35292e2f7e9f7d7120106ea9eee44907b454126f
Lockynbr <pauloluz566@gmail.com>
2024-08-29 18:28:28 -0300

    array basico

---
+++ aulas/aula29/index.js
@@ -0,0 +1,15 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+alunos.push('Luiza');
+alunos.push('Zé');
+
+//alunos.shift() //Remove o primeiro elemento do array.
+//alunos.pop() //Remove o ultimo elemento do array.
+//alunos.unshift('Paulo'); //Move o elemento para o começo do array.
+//alunos.push('Lucas'); //Adicionar mais um elemento no final do array.
+//alunos[alunos.length] = 'Luiza'; //Adicionar mais um elemento no final do array.
+//console.log(alunos[0].length); //Tamanho de um elemento no array.
+//delete alunos[1] //Deleta o elemento, mas no lugar dele irá ficar vazio.
+//console.log(alunos.slice(0, 2)); //Fatiamento de array.
+
+console.log(alunos);
