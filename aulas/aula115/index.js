fe4290528b4b3e49867eab914a3847b4f247c3a1
Lockynbr <pauloluz566@gmail.com>
2024-09-11 20:54:16 -0300

    babel modulo

---
+++ aulas/aula115/index.js
@@ -0,0 +1,9 @@
+// https://caniuse.com/ Site para ver em qual versão funciona algo nos navegadores.
+// https://babeljs.io/ Verifica a melhor maneira de utilizar o código para suportar em navegadores.
+
+class Pessoa {
+    constructor(nome, sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
