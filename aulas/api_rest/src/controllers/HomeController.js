7bbd38888b2314c26c8fcb030df0b83e220d20ec
Lockynbr <pauloluz566@gmail.com>
2024-10-16 21:52:46 -0300

    configurando e criando dados com sequelize

--- aulas/api_rest/src/controllers/HomeController.js
+++ aulas/api_rest/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Cristiano',
+      sobrenome: 'Gomes do Santos',
+      email: 'cristianocris12@gmail.com',
+      idade: 20,
+      peso: 80,
+      altura: 1.80,
     });
+    res.json(novoAluno);
   }
 }
 
