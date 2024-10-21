ae238760092e242382b82baa7c65314f12444d88
Lockynbr <pauloluz566@gmail.com>
2024-10-20 21:08:56 -0300

    atualizando home controller

--- aulas/api_rest/src/controllers/HomeController.js
+++ aulas/api_rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Cristiano',
-      sobrenome: 'Gomes do Santos',
-      email: 'cristianocris12@gmail.com',
-      idade: 20,
-      peso: 80,
-      altura: 1.80,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
