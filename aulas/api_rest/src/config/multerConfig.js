187e061b7f8aca08cd2788d9027c64dc75e9290e
Lockynbr <pauloluz566@gmail.com>
2024-10-21 22:33:29 -0300

    tratando os tipos de arquivos enviados

--- aulas/api_rest/src/config/multerConfig.js
+++ aulas/api_rest/src/config/multerConfig.js
@@ -4,6 +4,13 @@ import { extname, resolve } from 'path';
 const rand = () => Math.floor(Math.random() * 1000 + 1000);
 
 export default {
+  fileFilter: (req, file, callback) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return callback(new multer.MulterError('Extensão de arquivo inválida. O aqruivo deve ser PNG ou JPG.'));
+    }
+
+    return callback(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, callback) => {
       callback(null, resolve(__dirname, '..', '..', 'uploads'));
