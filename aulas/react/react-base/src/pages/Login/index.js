84b4a41895127611f07cc42ce8c212ed755b47df
Lockynbr <pauloluz566@gmail.com>
2024-11-03 21:42:18 -0300

    configurando axios

--- aulas/react/react-base/src/pages/Login/index.js
+++ aulas/react/react-base/src/pages/Login/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
-
 import { Title, Paragraph } from './styled';
 
+import axios from '../../services/axios';
+
 export default function Login() {
+  React.useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos');
+      const { data } = response;
+      console.log(data);
+    }
+
+    getData();
+  }, []);
   return (
     <Container>
       <Title isGray={false}>
